2ad04f6c3511658bcc6fee8452d2dfdcaebce7f0
aymannii <aymanaaay@gmail.com>
2021-06-14 19:49:21 +0200

    this is class Alarm.java

---
+++ src/Controller/Alarm.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import SignalProcessing.Pulse;
+import SignalProcessing.Signal;
+import SignalProcessing.SpO2;
+
+public class Alarm {
+
+    private boolean pause;
+    private boolean alarm;
+    private boolean deactivate;
+    private Pulse pulse;
+    private SpO2 oxygen;
+    private Limits limits;
+
+    public boolean isExceeded() { // The last value of SpO2 & pulse will be compared with the limits to check if they will be exceeded
+
+        if (pulse.getLatest() > limits.getLowerPulse() && pulse.getLatest() < limits.getUpperPulse()) {
+            return false;
+        } else if (oxygen.getLatest() > limits.getLowerSpO2() && oxygen.getLatest() < limits.getUpperSpO2()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void triggerAlarm(){
+
+    }
+
+}
